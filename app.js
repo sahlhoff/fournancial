d599cc1d19ddc482a508da83557306b522dbda97
Chad Sahlhoff <csahlhoff@gmail.com>
2013-01-05 10:30:10 -0800

    added mongolab

--- app.js
+++ app.js
@@ -76,6 +76,7 @@ var app = express();
 
 // configure Express
 app.configure(function() {
+  app.set('port', process.env.PORT || 5000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
   app.use(express.logger());
@@ -107,8 +108,7 @@ mongoose.connect('mongodb://localhost/fournancial');
 // Setup routes
 require('./routes')(app);
 
-var port = process.env.PORT || 3000;
 
-http.createServer(app).listen(port);
-
-
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
    'secrets' : {
     'clientId' : 'VD4N30FGBXSG42NMXX4SNTUCRAZVL2TAM12AN1R000QSEVN3',
     'clientSecret' : 'FEBNWSX1QIPXSQN5QKMU0TI3VUTWTOLNV2H3GJTVHER10FHQ',
-    'redirectUrl' : 'http://127.0.0.1:3000/auth/foursquare/callback'
+    'redirectUrl' : 'http://localhost:5000/auth/foursquare/callback'
   }
 };
