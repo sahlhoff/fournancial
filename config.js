0bb7332b7d4b514d909b8e9f481f8ba5b87888e7
Chad Sahlhoff <csahlhoff@gmail.com>
2013-01-08 18:10:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
    'secrets' : {
-    'clientId' : 'VD4N30FGBXSG42NMXX4SNTUCRAZVL2TAM12AN1R000QSEVN3',
-    'clientSecret' : 'FEBNWSX1QIPXSQN5QKMU0TI3VUTWTOLNV2H3GJTVHER10FHQ',
+    'clientId' : 'YOURCLIENTID',
+    'clientSecret' : 'YOURCLIENTSECRET',
     'redirectUrl' : 'http://fournancial.herokuapp.com/auth/foursquare/callback'
   }
 };
